6823d994786c9604420afc72d2d2db3c4555c0df
jinboy <zjdefine@163.com>
2019-12-30 17:33:00 +0800

    pojo

---
+++ C#/wavRecording/wavRecording/voice/config/UrlConstant.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wavRecording.config {
+    public class UrlConstant {
+        public static const string baseUrl  = "http://192.168.138.65:18801";
+    }
+}
